166e170c47ecc8be5eb021d7697924a0cd301021
Steven Zgaljic <szgaljic@jahnelgroup.com>
2026-01-23 14:52:45 -0500

    url and logos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
         title: 'Learn AI',
         logo: {
           alt: 'Jahnel Group Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
